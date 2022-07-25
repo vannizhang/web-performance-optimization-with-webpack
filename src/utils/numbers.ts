e6a8bdbfa4b0fee8eda6cb0f83ef643372bfa475
Jinnan Zhang <vannizhang@gmail.com>
2022-07-25 11:20:00 -0700

    update tsconfig

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,4 +1,4 @@
-export const add = (a:number, b:number)=>a+b;
+// export const add = (a:number, b:number)=>a+b;
 
 export const generateRandomNumber = (min=0, max=1)=>{
     const range = max - min;
